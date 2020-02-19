8f3371bdd2e44706f62d8fbe39ed1b18b97ca00d
Roberto  da Silva Goncalves <robertodasilvagoncalves@TAE466468.local>
2020-02-19 14:51:49 -0300

    atualizacao script de bhaskara para colocar itens em ordem crescente

--- big_data_ciencia_dados/formula_bhaskara.py
+++ big_data_ciencia_dados/formula_bhaskara.py
@@ -7,13 +7,15 @@ c = float(input("Digite o valor de c: "))
 delta = b ** 2 - 4 * a * c
 
 if delta < 0:
-        print('Esta equação não possui raízes reais')
+        print('esta equação não possui raízes reais')
 else:    
     raiz1 = (-b + math.sqrt(delta)) / (2 * a)
 
     if delta == 0:
-        print("A única raiz é: ", raiz1)
+        print("a raiz desta equação é", raiz1)
     else:
         raiz2 = (-b - math.sqrt(delta)) / (2 * a)
-        print("A primeira raiz é : ", raiz1)
-        print("A segunda raiz é : ", raiz2)
+        if raiz1 <= raiz2:
+            print("as raízes da equação são", raiz1, "e", raiz2)
+        else:
+            print("as raízes da equação são", raiz2, "e", raiz1)
